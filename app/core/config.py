355e78c1b509f77ba612c056732c9f45a9ef1cad
zhenia <zhenia@ubuntu20.04>
2024-04-16 14:15:04 +0300

    Connected postgresql database to project

--- app/core/config.py
+++ app/core/config.py
@@ -10,6 +10,16 @@ class Settings(BaseSettings):
     DEBUG: bool = True
     RELOAD: bool = True
 
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+    POSTGRES_HOST: str
+    POSTGRES_PORT: int
+
+    REDIS_URL: str
+    REDIS_PORT: str
+
+
     class Config:
         env_file = ".env"
         env_file_encoding = "utf-8"
--- app/routers/health_check.py
+++ app/routers/health_check.py
@@ -1,13 +1,22 @@
+import time
+
 from fastapi import APIRouter, Depends
 from fastapi.responses import JSONResponse
+from sqlalchemy.ext.asyncio import AsyncSession
+from typing import Annotated
+from app.db.database import get_async_session, check_db_connect
 
 router = APIRouter()
 
 @router.get("/")
-def health_check():
-    """returns json server status data"""
+async def health_check(session: Annotated[AsyncSession, Depends(get_async_session)]):
+    await check_db_connect(session)
     return JSONResponse({
       "status_code": 200,
       "detail": "ok",
       "result": "working"
       })
+
+
+
+
